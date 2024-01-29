d71a50c1eba6780bfdaac3b639a32aeee9a7aacf
Alejandro Gomez <alexserverone@gmail.com>
2024-01-29 16:43:36 -0600

    fixing tickers function, returning unfiltered array if query is empty. also reading default limit size from env params instead of hardcoding it. changing the params names, from query to search, and also adding limit and offset support.

--- jsondb/tickers.js
+++ jsondb/tickers.js
@@ -2,36 +2,46 @@
 
 import { readFile } from "node:fs/promises";
 
-export const getTickers = async ({ query, limit = 100, offset = 0 }) => {
+const API_REQUEST_LIMIT = process.env.API_REQUEST_LIMIT;
+
+export const getTickers = async ({
+  search,
+  limit = API_REQUEST_LIMIT,
+  offset = 0,
+}) => {
   const path = new URL("../data/tickers.json", import.meta.url);
   try {
     const file = await readFile(path, { encoding: "utf8" });
     const db = JSON.parse(file);
 
     // filter by query
-    const filtered = db.data?.filter(
-      (item) =>
-        item.name?.toLowerCase()?.includes(query?.toLowerCase()) ||
-        item.symbol?.toLowerCase()?.includes(query?.toLowerCase()) ||
-        item.stock_exchange?.country
-          ?.toLowerCase()
-          ?.includes(query?.toLowerCase()) ||
-        item.stock_exchange?.name
-          ?.toLowerCase()
-          ?.includes(query?.toLowerCase()) ||
-        item.stock_exchange?.acronym
-          ?.toLowerCase()
-          ?.includes(query?.toLowerCase())
-    );
+    const filtered = search
+      ? db.data?.filter(
+          (item) =>
+            item.name?.toLowerCase()?.includes(search?.toLowerCase()) ||
+            item.symbol?.toLowerCase()?.includes(search?.toLowerCase()) ||
+            item.stock_exchange?.country
+              ?.toLowerCase()
+              ?.includes(search?.toLowerCase()) ||
+            item.stock_exchange?.name
+              ?.toLowerCase()
+              ?.includes(search?.toLowerCase()) ||
+            item.stock_exchange?.acronym
+              ?.toLowerCase()
+              ?.includes(search?.toLowerCase())
+        )
+      : db.data;
+
+    const data = filtered.slice(Number(offset), Number(limit) + Number(offset));
 
     const pagination = {
       limit,
       offset,
-      count: filtered?.length,
+      count: data?.length,
       total: db.data?.length,
     };
 
-    return { pagination, data: filtered };
+    return { pagination, data };
   } catch (ex) {
     throw new Error(ex);
   }
--- routes/tickers.js
+++ routes/tickers.js
@@ -5,10 +5,14 @@ export const tickerRoutes = (app, fs) => {
   const dataPath = "./data/tickers.json";
 
   // READ
+  // Params:
+  // search: string; a string to search and compare against symbol, name, stock exchange name, and stock exchange country
+  // limit: string; the number of rows to fetch
+  // offset: string; the starting point to fetch rows
   app.get("/tickers", async (req, res) => {
-    const { query } = req.query;
+    const { search, limit, offset } = req.query;
     try {
-      const data = await getTickers({ query });
+      const data = await getTickers({ search, limit, offset });
       res.send(data);
     } catch (ex) {
       throw new Error(ex);
